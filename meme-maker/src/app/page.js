4960c8b0d682a806dd67cf9fcece7dc2c1598206
Walterhart <walterwhart1@gmail.com>
2023-06-12 01:24:21 -0400

    Created better navbar

--- meme-maker/src/app/page.js
+++ meme-maker/src/app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from '../styles/page.module.css'
 
 export default function Home() {
   return (
+
     <main className={styles.main}>
-     <h2> Hello</h2>
+     <h2> image</h2>
     </main>
   )
 }
---
+++ meme-maker/src/components/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="container container-nav">
+            <div className="site-title">
+            <h1>Meme Maker</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Site</a></li>
+                </ul>
+            </nav>
+            </div>
+        </header>
+      );
+}
+ 
+export default Header;    
